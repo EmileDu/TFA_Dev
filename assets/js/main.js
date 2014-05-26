b4eb6a88c24105b95d6c8885e228fe9f68abafdc
Emile Duval <duval.emile@gmail.com>
2014-05-26 22:37:29 +0200

    Start stylizing and add overlay grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,11 +21,64 @@
 
 	});
 
+	
+	definegrid = function() {
+		var browserWidth = $(window).width(); 
+		if (browserWidth >= 1024) 
+		{
+			pageUnits = 'px';
+			colUnits = 'px';
+			pagewidth = 960;
+			columns = 12;
+			columnwidth = 55.25;
+			gutterwidth = 27;
+			pagetopmargin = 0;
+			rowheight = 27;
+			gridonload = 'on';
+			makehugrid();
+		} 
+		if (browserWidth <= 1023) 
+		{
+			pageUnits = '%';
+			colUnits = '%';
+			pagewidth = 94;
+			columns = 6;
+			columnwidth = 48;
+			gutterwidth = 4;
+			pagetopmargin = 35;
+			rowheight = 20;
+			gridonload = 'on';
+			makehugrid();
+		}
+		if (browserWidth <= 640) 
+		{
+			pageUnits = '%';
+			colUnits = '%';
+			pagewidth = 96;
+			columns = 2;
+			columnwidth = 49;
+			gutterwidth = 2;
+			pagetopmargin = 35;
+			rowheight = 20;
+			gridonload = 'on';
+			makehugrid();
+		}
+	}
 
 	$(document).ready(function(){
 		
 		SHORT_NAME.init();
-
+		
+		definegrid();
+		setgridonload();  
+			
 	});//close document ready
+	
+	$(window).resize(function() {
+		
+		definegrid();
+		setgridonresize();
+		
+	});
 
-})(jQuery)
+})(jQuery);
