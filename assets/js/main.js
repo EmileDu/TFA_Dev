819f45972ee62f89667a708b113c728859a9b44f
Emile Duval <duval.emile@gmail.com>
2014-06-14 23:06:19 +0200

    almost the end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,32 @@
 (function ($) {
-	valid = true;
+	
+	//-----------------
+	/*    Variable   */
+	//-----------------
+	
+	var valid = true,
+			rootUrl = History.getRootUrl();
+	
+	
+	//-----------------
+	/*    Function   */
+	//-----------------
+	
+	
+	// HTML Helper
+	var documentHtml = function(html){
+		// Prepare
+		var result = String(html)
+			.replace(/<\!DOCTYPE[^>]*>/i, '')
+			.replace(/<(html|head|body|title|meta|script|link)([\s\>])/gi,'<div class="document-$1"$2')
+			.replace(/<\/(html|head|body|title|meta|script)\>/gi,'</div>')
+		;
+
+		// Return
+		return $.trim(result);
+	};
+	
+	// Define grid setup for each resolution ( desktop, tablet, mobile )
 	definegrid = function () {
 		var browserWidth = $(window).width();
 		if (browserWidth >= 1025) {
@@ -39,7 +66,57 @@
 			makehugrid();
 		}
 	}
-
+	
+	// Show the loading animation on loading start
+	function startLoading(){
+		console.log('start loading');
+		$('#loading').show();
+	}
+	
+	
+	// 
+	function progressLoading(){
+		console.log('progress loading');
+	}
+	
+	
+	// When loading is finish succesfully
+	function doneLoading(data, textStatus, jqXHR, url, title, relativeUrl){
+		console.log('done loading');
+		var $data = $(documentHtml(data)),
+				$dataBody = $data.find('.document-body:first'),
+				$dataContent = $dataBody.find('#content').filter(':first'),
+				$dataContent = $dataContent[0].innerHTML,
+				curpage = window.location.pathname.split('/').pop(),
+				homepage = rootUrl;
+		$('#content').html($dataContent);
+		$
+		$('body').removeClass().addClass(title);
+		
+		document.title = $data.find('.document-title:first').text();
+		
+		// Inform Google Analytics of the change
+		if ( typeof window._gaq !== 'undefined' ) {
+			window._gaq.push(['_trackPageview', relativeUrl]);
+		}
+		
+	}
+	
+	// When loading is finish but has fail
+	function failLoading(data, textStatus, jqXHR, url){
+		console.log('fail loading');
+		document.location.href = url;
+		return false;
+	}
+	
+	
+	// Hide loading animation on loading end
+	function finishLoading(){
+		console.log('finish loading');
+		$('#loading').hide();	
+	}
+	
+	
 	function checkValue(name, value){
 		var message = '';
 		switch(name){
@@ -70,101 +147,76 @@
 		}
 		return message;
 	}
-	
-	function startLoading(){
-		console.log('start loading');
-		$('.loading').show();
-	}
-	
-	function progressLoading(){
-		console.log('progress loading');
-	}
-	
-	function doneLoading(data, url, name, newClass){
-		console.log('done loading');
-		var title = $(data)[35].innerHTML;
-		var content = $(data)[81].innerHTML;
-		url = url.split('.')[0];
-		document.title = title;
-		$('#content').html(content);
-		$('body').attr('class', newClass);
-		history.pushState({key: 'case-study'}, name, url);
-		finishLoading();
-	}
-	
-	function failLoading(data){
-		console.log('fail loading');
-		finishLoading();
-	}
-	
-	function finishLoading(){
-		console.log('finish loading');
-		$('.loading').hide();	
-	}
-	
-	function preinit() {
-		$('.loading').show();
-		$(window).load(function () {
-			init();
-			$('.loading').fadeOut();
-		});
-	}
 
-	function init() {
-		history.pushState(null, 'index', document.location.origin);
-		definegrid();
-		setgridonload();
+	// Update header Height to always match with baseline
+	function headerHeight(){
 		var $headerHeight = $('.header').outerHeight();
 		if ($headerHeight % 27 != 0) {
 			$('.header').outerHeight(Math.ceil($headerHeight / 27) * 27);
 		}
+	}
+	
+	// Call when DOM is ready and wait for document fully loaded, to call init function and end loading
+	function preinit(){
+		$(window).load(function () {
+			init();
+			finishLoading();
+		});
+	}
+	
 
-		// Load page in ajax
+	function init() {
+		
+		definegrid();
+		setgridonload();
+		
+		headerHeight();
+
+		//Load page in ajax
 		$('body').on('click tap', '[data-ajaxLink]',function(ev){
-			var url = $(this).attr('href');
-			var name = $(this).attr('data-name');
-			var newClass = $(this).attr('data-class');
+			var $this = $(this),
+					url = $this.attr('href'),
+					title = $this.attr('title') || null;
+			
+			if ( ev.which == 2 || ev.metaKey ) { // If voluntarily opens in a new window ( cmd + click ) 
+				return true; 
+			}
+			
+			History.pushState(null, title, url);
+			ev.preventDefault();
+			return false;
+			
+		});
+								 
+		$(window).bind('statechange', function(ev){
+			var State = History.getState(),
+					url = State.url,
+					title = State.title,
+					relativeUrl = url.replace(rootUrl,'');
+			
 			$.ajax({
-				type: 'GET',
-				url: $(this).attr('href'),
-				dataType: 'HTML',
-				data: {},
-				beforeSend: startLoading(),						
+				url: url,
+				beforeSend: startLoading(),
 				xhrFields:{
 					onprogress: progressLoading()
-				}
+				},
 			})
-			.done(function(data){
-				doneLoading(data, url, name, newClass);
+			.done(function(data, textStatus, jqXHR){
+				doneLoading(data, textStatus, jqXHR, url, title, relativeUrl);
 			})
-			.fail(function(data){
-				failLoading(data)
-			});
-			return false;
-		});
+			.fail(function(data, textStatus, jqXHR){
+				failLoading(data, textStatus, jqXHR, url);
+			})
+			.always( finishLoading() );
+			
+		});					 
 		
-		window.onpopstate = function(ev){
-			if (event.state == null){
-				history.pushState(null, 'index', document.location.origin);
-			}
-		}
-		$('input, textarea').on('focusin', function(){
-			if ($('#form--submit').hasClass('is-done')){
-				$('#form--submit').removeClass('is-done')
-			} else if($('#form--submit').hasClass('is-fail')){
-				$('#form--submit').removeClass('is-fail');
-			}
-		});
+		// Formulaire de contact
+	
 		
 		// Replace placeholder if enter value is not valid
-		$('input, textarea').on('focusout', function(ev){
-			var output = checkValue($(this)[0].name, $(this)[0].value);
-			$(this).css('border','1xp solid red');
-			$(this).attr('placeholder', output);
-		});
-				
+		$('input, textarea').on('focusout', function(ev){ $(this).attr('placeholder', checkValue($(this)[0].name, $(this)[0].value)) });
 		
-		// Formulaire de contact 
 		$('#form--submit').on('click', function (ev) {
 			ev.preventDefault();
 			$('#contact-form').trigger('submit');
@@ -208,15 +260,52 @@
 			}
 			return false;
 		});
+		
+		$('input, textarea').on('focusin', function(){
+			if ($('#form--submit').hasClass('is-done')){
+				$('#form--submit').removeClass('is-done')
+			} else if($('#form--submit').hasClass('is-fail')){
+				$('#form--submit').removeClass('is-fail');
+			}
+		});
 	}
 	
-	$(document).ready(function () {
+	
+	// When DOM is ready, show loading animation and call preinit function
+	$(document).ready(function () { 
+		
+		startLoading();
 		preinit();
-	}); //close document ready
-
+		
+	}); 
+	
+	// On resize call grid function for update him
 	$(window).resize(function () {
 		definegrid();
 		setgridonresize();
 	});
 
 })(jQuery);
+
+
+
+
+
+
+
+
+//	startLoading();
+//
+//$('html body').hide()
+//$(document).ready(function () { 
+//	$('html body').fadeOut('slow', function() {
+//		$('a[href], button[href]').click(function(event) {
+//			var url = $(this).attr('href');
+//			if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+//			event.preventDefault();
+//			$('html body').fadeIn('slow', function() {
+//				window.location = url;
+//			});
+//		});
+//	});
+//});
